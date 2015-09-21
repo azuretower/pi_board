af03b530b058a4bec406825e27505ad2861cc04c
Ryan <ryan.merrill215@gmail.com>
2015-09-21 12:29:38 -0600

    testing gpio

--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import RPi.GPIO as GPIO
 
 def home(request):
     context = {}
+    context['version'] = GPIO.VERSION
     return render(request, 'home.html', context)
+
+def light_on(request):
+    context = {}
+
+
+    return HttpResponse(status=200)
+
+def light_off(request):
+    context = {}
+
+    
+    return HttpResponse(status=200)
